ff76a00c6edae58654751485c4220d83577cae38
Mateusz <kaawkamateusz@gmail.com>
2018-07-30 02:23:23 +0200

    some fix with respons

--- sources/scripts/main.js
+++ sources/scripts/main.js
@@ -21,3 +21,13 @@ menuButton.addEventListener('click', () => menuToggle() )
 
 
 var scroll = new SmoothScroll('a[href*="#"]');
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('meet us here')
+    .openPopup();
